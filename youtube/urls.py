c69b54dbbe30334e208d42e2196afb1082a2710a
user <user@usernoMacBook-Air.local>
2024-07-22 10:21:12 +0900

    commit4-3

--- youtube/urls.py
+++ youtube/urls.py
@@ -5,6 +5,7 @@ app_name='youtube'
 
 urlpatterns=[
     path('',views.IndexView.as_view(),name='index'),
+    path('category/<int:pk>',views.CategoryView.as_view(),name='category'),
     path('create/',views.CreateView.as_view(),name='create'),
     path('detail/<int:pk>',views.DetailView.as_view(),name='detail'),
 ]
--- youtube/views.py
+++ youtube/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
 from django.views import generic
-from .models import Post
+from django.shortcuts import get_object_or_404
+from .models import Post,Category
 from django.urls import reverse_lazy
 from .forms import YoutubeCreateForm
+from django.db.models import Q
 
 class IndexView(generic.ListView):
     model=Post
+    
+    def get_queryset(self):
+        queryset=Post.objects.order_by('-created_at')
+        keyword=self.request.GET.get('keyword')
+        if keyword:
+            queryset=queryset.filter(
+                Q(title_icontains=keyword)|Q(text_icontains=keyword)
+            )
+        return queryset
+
+class CategoryView(generic.ListView):
+    model=Post
+    
+    def get_queryset(self):
+        category=get_object_or_404(Category,pk=self.kwargs['pk'])
+        queryset=Post.objects.order_by('-created_at').filter(category=category)
+        return queryset
+        
+    
     
 class CreateView(generic.CreateView):
     model=Post
